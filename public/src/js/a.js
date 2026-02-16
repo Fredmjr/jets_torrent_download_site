b4c675361c2851d0744c5138f14f52722f3957af
Fredmjr <fredmjr@gmail.com>
2026-02-16 17:21:35 +0200

    tkn failure v2

---
+++ public/src/js/a.js
@@ -0,0 +1,14 @@
+fetch("/usr/crtusr", {
+  method: "GET",
+})
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.usr_id_tkn) {
+      //cookie no expry voz it just a simple and quick setup
+      console.log("fetched: ", data.usr_id_tkn);
+      document.cookie =
+        `usr_id_tkn=${encodeURIComponent(data.usr_id_tkn)};` +
+        `Secure; SameSite=Strict; path=/`;
+    }
+  })
+  .catch((error) => console.error(error));
